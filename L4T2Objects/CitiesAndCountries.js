3cbaa691c7a7ca4f32c86400c678e385398a78f4
9045857 <9045857@mail.ru>
2020-02-26 14:08:18 +0700

    Задача объекты. Исправления к первым замечаниям.

--- L4T2Objects/CitiesAndCountries.js
+++ L4T2Objects/CitiesAndCountries.js
@@ -1,115 +1,164 @@
-﻿//Создайте массив объектов стран(пусть будет несколько
-//стран)
-//•
-//У страны есть название и список городов(пусть будет по
-//несколько городов)
-//•
-//У города есть название и численность населения
-//•
-//Найдите страну страны с максимальным количеством
-//городов
-//•
-//Получите объект с информацией по всем странам такого
-//вида: ключ название страны, значение суммарная
-//численность по стране
-//•
-//Оформите код в виде функций
-
-var portugal = {
-    name: "Португалия",
-    cities: [
-        { name: "Лиссабон", population: 504718 },
-        { name: "Порту", population: 214349 },
-        { name: "Коимбра", population: 105842 }
-    ]
-};
-
-var peru = {
-    name: "Перу",
-    cities: [
-        { name: "Лима", population: 9562280 },
-        { name: "Куско", population: 428450 },
-        { name: "Арекипа", population: 978009 },
-        { name: "Трухильо", population: 645000 }
-    ]
-};
-
-var argentina = {
-    name: "Аргентина",
-    cities: [
-        { name: "Буэнос-Айрес", population: 2890000 },
-        { name: "Сальта", population: 535050 },
-        { name: "Мар-дель-Плата", population: 593000 },
-        { name: "Мендоса", population: 112900 },
-        { name: "Кордова", population: 1391000 }
-    ]
-};
-
-var czech = {
-    name: "Чехия",
-    cities: [
-        { name: "Прага", population: 1294513 },
-        { name: "Конице", population: 2803 },
-        { name: "Острава", population: 290450 },
-        { name: "Кралики", population: 4224 },
-        { name: "Пльзень", population: 170936 },
-        { name: "Либерец", population: 103979 },
-        { name: "Брно", population: 379527 }
-    ]
-};
-
-var thailand = {
-    name: "Таиланд",
-    cities: [
-        { name: "Аюттхая", population: 55025 },
-        { name: "Кхонкэн", population: 120857 },
-        { name: "Лампанг", population: 60591 },
-        { name: "Пхукет", population: 74361 },
-        { name: "Сонгкхла", population: 75408 },
-        { name: "Транг", population: 59846 },
-        { name: "Чианграй", population: 69261 }
-    ]
-};
-
-var countries = [portugal, peru, argentina, czech, thailand];
-
-function GetCountriesNameWithMaxCitiesCount(countriesArray) {
-    var countriesNameWithMaxCitiesCount = [];
-    var countryWithMaxCitiesCount = countriesArray[0];
-
-    countriesNameWithMaxCitiesCount.push(countryWithMaxCitiesCount.name);
-
-    for (var i = 1; i < countriesArray.length; i++) {
-        if (countryWithMaxCitiesCount.cities.length < countriesArray[i].cities.length) {
-
-            countryWithMaxCitiesCount = countriesArray[i];
-            countriesNameWithMaxCitiesCount.splice(0, countriesNameWithMaxCitiesCount.length);
-            countriesNameWithMaxCitiesCount.push(countryWithMaxCitiesCount.name);
-
-        } else if (countryWithMaxCitiesCount.cities.length === countriesArray[i].cities.length) {
-
-            countriesNameWithMaxCitiesCount.push(countriesArray[i].name);
-        }
+﻿var countries = [
+    {
+        name: "Португалия",
+        cities: [
+            {
+                name: "Лиссабон",
+                population: 504718
+            },
+            {
+                name: "Порту",
+                population: 214349
+            },
+            {
+                name: "Коимбра",
+                population: 105842
+            }
+        ]
+    },
+    {
+        name: "Перу",
+        cities: [
+            {
+                name: "Лима",
+                population: 9562280
+            },
+            {
+                name: "Куско",
+                population: 428450
+            },
+            {
+                name: "Арекипа",
+                population: 978009
+            },
+            {
+                name: "Трухильо",
+                population: 645000
+            }
+        ]
+    },
+    {
+        name: "Аргентина",
+        cities: [
+            {
+                name: "Буэнос-Айрес",
+                population: 2890000
+            },
+            {
+                name: "Сальта",
+                population: 535050
+            },
+            {
+                name: "Мар-дель-Плата",
+                population: 593000
+            },
+            {
+                name: "Мендоса",
+                population: 112900
+            },
+            {
+                name: "Кордова",
+                population: 1391000
+            }
+        ]
+    },
+    {
+        name: "Чехия",
+        cities: [
+            {
+                name: "Прага",
+                population: 1294513
+            },
+            {
+                name: "Конице",
+                population: 2803
+            },
+            {
+                name: "Острава",
+                population: 290450
+            },
+            {
+                name: "Кралики",
+                population: 4224
+            },
+            {
+                name: "Пльзень",
+                population: 170936
+            },
+            {
+                name: "Либерец",
+                population: 103979
+            },
+            {
+                name: "Брно",
+                population: 379527
+            }
+        ]
+    },
+    {
+        name: "Таиланд",
+        cities: [
+            {
+                name: "Аюттхая",
+                population: 55025
+            },
+            {
+                name: "Кхонкэн",
+                population: 120857
+            },
+            {
+                name: "Лампанг",
+                population: 60591
+            },
+            {
+                name: "Пхукет",
+                population: 74361
+            },
+            {
+                name: "Сонгкхла",
+                population: 75408
+            },
+            {
+                name: "Транг",
+                population: 59846
+            },
+            {
+                name: "Чианграй",
+                population: 69261
+            }
+        ]
     }
+];
 
-    return countriesNameWithMaxCitiesCount;
-};
+function getCountriesWithMaxCitiesCount(countriesArray) {
+    var maxCitiesCount = Math.max.apply(null, countriesArray.map(function (item) { return item.cities.length }));
 
-function GetCountryPopulationDictionary(countriesArray) {
-    var countryPopulationDictionary = [];
+    return countriesArray.filter(function (item) {
+        return item.cities.length === maxCitiesCount
+    });
+}
+
+function getCountryPopulationDictionary(countriesArray) {
+    var countryPopulationDictionary = {};
 
     countriesArray.forEach(function (item) {
-
-        var initialValue = 0;
         var sum = item.cities.reduce(function (accumulator, currentValue) {
             return accumulator + currentValue.population;
-        }, initialValue);
+        }, 0);
 
         countryPopulationDictionary[item.name] = sum;
     });
 
     return countryPopulationDictionary;
-};
+}
 
-console.log(GetCountriesNameWithMaxCitiesCount(countries));
-console.log(GetCountryPopulationDictionary(countries));
+(function (array) {
+    console.log("Страны:");
+    console.log(array);
+
+    console.log("Страна(ы) с максимальным количеством городов:");
+    console.log(getCountriesWithMaxCitiesCount(array));
+
+    console.log("Oбъект с информацией по всем странам такого вида: ключ название страны, значение суммарная численность по стране:");
+    console.log(getCountryPopulationDictionary(array));
+}(countries));
---
+++ Test/TestScript.js
@@ -0,0 +1,60 @@
+﻿var arrArticles = [
+    {
+        href: "/article7",
+        title: "Статья 7",
+        likes: 15,
+        comments: [
+            {
+                1: 1,
+                2: "2"
+            },
+            {
+                1: 3,
+                2: "3"
+            }
+        ]
+    },
+    {
+        href: "/article1",
+        title: "Статья 1",
+        likes: 15,
+        comments: [
+            {
+                1: 1,
+                2: "2"
+            },
+            {
+                1: 3,
+                2: "3"
+            },
+            {
+                1: 89,
+                2: "9"
+            }
+        ]
+    },
+    {
+        href: "/article5",
+        title: "Статья 5",
+        likes: 3,
+        comments: [{}, {}]
+    },
+    {
+        href: "/article3",
+        title: "Статья 3",
+        likes: 20,
+        comments: [{}, {}]
+    }
+];
+
+function compareTitle(articleA, articleB) {
+    if (articleA.title < articleB.title)
+        return -1;
+    if (articleA.title > articleB.title)
+        return 1;
+    return 0;
+}
+
+arrArticles.sort(compareTitle);
+
+console.log(arrArticles);
