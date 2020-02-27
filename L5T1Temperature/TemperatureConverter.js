0109e43b18e92424bf99ea549dfe02d63d5fb0f3
9045857 <9045857@mail.ru>
2020-02-28 02:30:54 +0700

    температурный конвертор

---
+++ L5T1Temperature/TemperatureConverter.js
@@ -0,0 +1,39 @@
+﻿
+//• Сделайте страницу для перевода температур
+//• Сделайте поле ввода для температуры в цельсиях
+//• И по нажатию на кнопку, чтобы температура переводилась
+//в шкалы кельвина и фаренгейта, и результаты выводились
+//на страницу
+
+function convertToKelvin(celsiusDegrees) {
+    return celsiusDegrees + 273.15;
+}
+
+function convertToFahrenheit(celsiusDegrees) {
+    return (celsiusDegrees * 9 / 5) + 32;
+}
+
+(function () {
+    var button = document.querySelector(".convert-button");
+    button.addEventListener("click",
+        function () {
+            var inputText = document.querySelector(".input-temperature>input").value;
+            
+            if (inputText === "") {
+                document.querySelector(".kelvin").textContent = "";
+                document.querySelector(".fahrenheit").textContent = "";
+
+                return;
+            }
+
+            var celsiusDegree = parseFloat(inputText);
+
+            if (!isNaN(celsiusDegree)) {
+                document.querySelector(".kelvin").textContent = convertToKelvin(celsiusDegree);
+                document.querySelector(".fahrenheit").textContent = convertToFahrenheit(celsiusDegree);
+            } else {
+                document.querySelector(".kelvin").textContent = "некорректное введенное значение";
+                document.querySelector(".fahrenheit").textContent = "некорректное введенное значение";
+            }
+        });
+}());
