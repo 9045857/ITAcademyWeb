4ecd967279b51b8de2193bd76128d42716ca0712
9045857 <9045857@mail.ru>
2020-02-28 14:45:30 +0700

    в температурах добавил нажатие на кнопку

--- L5T1Temperature/TemperatureConverter.js
+++ L5T1Temperature/TemperatureConverter.js
@@ -13,27 +13,39 @@ function convertToFahrenheit(celsiusDegrees) {
     return (celsiusDegrees * 9 / 5) + 32;
 }
 
+function convertTemperature() {
+    var inputText = document.querySelector(".input-temperature>input").value;
+
+    if (inputText === "") {
+        document.querySelector(".kelvin").textContent = "";
+        document.querySelector(".fahrenheit").textContent = "";
+
+        return;
+    }
+
+    var celsiusDegree = parseFloat(inputText);
+
+    if (!isNaN(celsiusDegree)) {
+        document.querySelector(".kelvin").textContent = convertToKelvin(celsiusDegree);
+        document.querySelector(".fahrenheit").textContent = convertToFahrenheit(celsiusDegree);
+    } else {
+        document.querySelector(".kelvin").textContent = "некорректное введенное значение";
+        document.querySelector(".fahrenheit").textContent = "некорректное введенное значение";
+    }
+}
+
 (function () {
     var button = document.querySelector(".convert-button");
-    button.addEventListener("click",
-        function () {
-            var inputText = document.querySelector(".input-temperature>input").value;
-            
-            if (inputText === "") {
-                document.querySelector(".kelvin").textContent = "";
-                document.querySelector(".fahrenheit").textContent = "";
-
-                return;
-            }
-
-            var celsiusDegree = parseFloat(inputText);
-
-            if (!isNaN(celsiusDegree)) {
-                document.querySelector(".kelvin").textContent = convertToKelvin(celsiusDegree);
-                document.querySelector(".fahrenheit").textContent = convertToFahrenheit(celsiusDegree);
-            } else {
-                document.querySelector(".kelvin").textContent = "некорректное введенное значение";
-                document.querySelector(".fahrenheit").textContent = "некорректное введенное значение";
-            }
-        });
+    button.addEventListener("click", function() {
+         convertTemperature();
+    });
 }());
+
+(function () {
+    document.querySelector(".input-temperature>input").addEventListener("keydown", function (e) {
+        if (e.keyCode === 13) {
+            convertTemperature();
+        }
+    });
+}());
+
