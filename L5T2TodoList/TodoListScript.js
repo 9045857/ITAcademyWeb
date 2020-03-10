8381b39f339989bbf0674698477c079e02201c32
9045857 <9045857@mail.ru>
2020-03-10 17:45:00 +0700

    тудулист переделал на инлинеар стили часть функций, которые через js добавляются

--- L5T2TodoList/TodoListScript.js
+++ L5T2TodoList/TodoListScript.js
@@ -1,12 +1,4 @@
-﻿
-//2. Здесь лучше не использовать position fixed.
-//    Сейчас страница не скроллится по вертикали если малая высота окна браузера
-
-//3. Вместо работы с атрибутами disabled и class лучше работать со свойствами DOM элементов
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
+﻿document.addEventListener("DOMContentLoaded", function () {
     var tasksList = document.getElementById("tasks-list");
     var newTask = document.getElementById("input-task-text");
 
@@ -21,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var li = document.createElement("li");
 
-        li.innerHTML = "<button type='button' title='Отметка о выполнении задачи' class='check-box unchecked'><img src='Images/check-box.png'></button>" +
-            "<input type='text' class='task' value='' disabled='disabled'>" +
+        li.innerHTML = "<button type='button' title='Отметка о выполнении задачи' class='check-box'><img src='Images/check-box.png'></button>" +
+            "<input type='text' class='task' value=''>" +
             "<button type='button' title='Сохранить изменения' class='save-correction task-button'><img src='Images/save.png'></button>" +
             "<button type='button' title='Не сохранять изменения' class='not-save-correction task-button'><img src='Images/notSave.png'></button>" +
             "<button type='button' title='Редактировать задачу' class='correct task-button'><img src='Images/pencil.png'></button>" +
@@ -30,46 +22,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var input = li.getElementsByTagName("input")[0];
         input.value = newTaskText;
+        input.disabled = true;
 
         tasksList.appendChild(li);
 
         var liButtons = li.getElementsByTagName("button");
 
         var checkButton = liButtons[0];
+        checkButton.isChecked = false;
+
+        var checkImg = checkButton.getElementsByTagName("img")[0];
+        checkImg.style.display = "none";
+
         checkButton.addEventListener("click", function () {
-            if (this.classList.contains("unchecked")) {
-                this.setAttribute("class", "check-box");
+            checkButton.isChecked = !checkButton.isChecked;
+
+            if (this.isChecked) {
+                checkImg.style.display = "";
             } else {
-                this.setAttribute("class", "check-box unchecked");
+                checkImg.style.display = "none";
             }
         });
 
-        var taskText = newTaskText;
+        var taskTextBeforeCorrection = newTaskText;
 
         var correctButton = liButtons[3];
         correctButton.addEventListener("click", function () {
-            input.removeAttribute("disabled", "disabled");
-            taskText = input.value;
+            input.disabled = false;
+            taskTextBeforeCorrection = input.value;
         });
 
         var saveButton = liButtons[1];
         saveButton.addEventListener("click", function () {
-            input.setAttribute("disabled", "disabled");
+            input.disabled = true;
         });
 
         var notSaveButton = liButtons[2];
         notSaveButton.addEventListener("click", function () {
-            input.setAttribute("disabled", "disabled");
-            input.value = taskText;
+            input.value = taskTextBeforeCorrection;
+            input.disabled = true;
         });
 
         var preDeleteButton = liButtons[4];
+        preDeleteButton.isPressed = false;
         preDeleteButton.addEventListener("click", function () {
-            if (li.classList.contains("is-delete-li")) {
+            if (preDeleteButton.isPressed) {
                 return;
             }
-            
-            li.setAttribute("class", "is-delete-li");
+
+            preDeleteButton.isPressed = true;
+
+            li.style.backgroundColor = "#fffabe";
 
             var deleteMenuHtml = document.createElement("div");
             deleteMenuHtml.setAttribute("class", "delete-menu");
@@ -77,17 +80,21 @@ document.addEventListener("DOMContentLoaded", function () {
                 "<button type='button' class='delete-menu-button yes-delete'>Да</button>" +
                 "<button type='button' class='delete-menu-button no-delete'>Нет</button>";
 
-            var deleteButton = deleteMenuHtml.getElementsByClassName("yes-delete")[0];
+            var deleteMenuButtons = deleteMenuHtml.getElementsByTagName("button");
+
+            var deleteButton = deleteMenuButtons[0];
             deleteButton.addEventListener("click", function () {
                 deleteMenuHtml.parentNode.removeChild(deleteMenuHtml);
                 li.parentNode.removeChild(li);
             });
 
-            var notDeleteButton = deleteMenuHtml.getElementsByClassName("no-delete")[0];
+            var notDeleteButton = deleteMenuButtons[1];
             notDeleteButton.addEventListener("click", function () {
                 var menu = notDeleteButton.parentNode;
                 menu.parentNode.removeChild(menu);
-                li.removeAttribute("class");
+
+                li.style.backgroundColor = "";
+                preDeleteButton.isPressed = false;
             });
 
             li.after(deleteMenuHtml);
