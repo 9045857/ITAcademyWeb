4011b43aa8d7e82a01c5aabfe56035ccb3e43868
9045857 <9045857@mail.ru>
2020-03-14 20:31:24 +0700

    поправлен экран белый блокировки

---
+++ L7T1Underscore/L7T1Underscore.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+var people = [
+    { age: 39, name: "Виктор", lastName: "Болотин" }, { age: 21, name: "Фёдор", lastName: "Левин" },
+    { age: 24, name: "Камила", lastName: "Кабирова" }, { age: 23, name: "Анастасия", lastName: "Лопачева" },
+    { age: 15, name: "Алиса", lastName: "Галиева" }, { age: 32, name: "Илья", lastName: "Кардаков" },
+    { age: 15, name: "Алиса", lastName: "Чекурашева" }, { age: 38, name: "Элина", lastName: "Адабир" },
+    { age: 35, name: "Елена", lastName: "Дзюбина" }, { age: 39, name: "Юлия", lastName: "Исаева" }
+];
+
+
+
+
+
+
+});
