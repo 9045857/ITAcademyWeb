8f0abbb378d0db0e05ef7868b114d35c4ae8965d
9045857 <9045857@mail.ru>
2020-03-19 15:42:01 +0700

    Конвертация доработанная

--- L5T1Temperature/TemperatureConverter.js
+++ L5T1Temperature/TemperatureConverter.js
@@ -7,39 +7,77 @@
         return (celsiusDegrees * 9 / 5) + 32;
     }
 
-    function convertTemperature(kelvinSpanText, fahrenheitSpanText, inputText) {
-        if (inputText.value === "") {
+    function addWarningClass(warningText, inputTemperature, warningClass) {
+        if (!warningText.classList.contains(warningClass)) {
+            warningText.classList.add(warningClass);
+        }
+
+        if (!inputTemperature.classList.contains(warningClass)) {
+            inputTemperature.classList.add(warningClass);
+        }
+    }
+
+    function removeWarningClass(warningText, inputTemperature, warningClass) {
+        if (warningText.classList.contains(warningClass)) {
+            warningText.classList.remove(warningClass);
+        }
+
+        if (inputTemperature.classList.contains(warningClass)) {
+            inputTemperature.classList.remove(warningClass);
+        }
+    }
+
+    function convertTemperature(kelvinSpanText, fahrenheitSpanText, inputCelsiusTemperature, warning, warningClass) {
+        if (inputCelsiusTemperature.value === "") {
             kelvinSpanText.textContent = "";
             fahrenheitSpanText.textContent = "";
+
+            var emptyInputWarning = "Введите значение температуры!";
+            warning.textContent = emptyInputWarning;
+
+            addWarningClass(warning, inputCelsiusTemperature, warningClass);
+
             return;
         }
 
-        var celsiusDegree = parseFloat(inputText.value);
+        removeWarningClass(warning, inputCelsiusTemperature, warningClass);
+
+        var celsiusDegree = parseFloat(inputCelsiusTemperature.value);
 
         if (!isNaN(celsiusDegree)) {
             kelvinSpanText.textContent = convertToKelvin(celsiusDegree);
             fahrenheitSpanText.textContent = convertToFahrenheit(celsiusDegree);
+
         } else {
+            var notCorrectInputWarning = "Недопустимые символы ввода!";
+            warning.textContent = notCorrectInputWarning;
+
+            addWarningClass(warning, inputCelsiusTemperature, warningClass);
+
             kelvinSpanText.textContent = "¯\\_(ツ)_/¯";
             fahrenheitSpanText.textContent = "¯\\_(ツ)_/¯";
         }
     }
 
-    (function () {
-        var kelvinSpan = document.querySelector("#kelvin");
-        var fahrenheitSpan = document.querySelector("#fahrenheit");
-        var inputCelsius = document.querySelector(".input-white-area input");
+    var kelvinSpan = document.querySelector("#kelvin");
+    var fahrenheitSpan = document.querySelector("#fahrenheit");
+    var inputCelsius = document.querySelector("#input-celsius");
+    var warningP = document.querySelector("#warning-massage");
+    var notCorrectInputWarningClass = "try-not-correct-input";
 
-        var button = document.querySelector("#convert-button");
-        button.addEventListener("click", function () {
-            convertTemperature(kelvinSpan, fahrenheitSpan, inputCelsius);
-        });
+    var button = document.querySelector("#convert-button");
+    button.addEventListener("click", function () {
+        convertTemperature(kelvinSpan, fahrenheitSpan, inputCelsius, warningP, notCorrectInputWarningClass);
+    });
 
-        inputCelsius.addEventListener("keydown", function (e) {
-            if (e.keyCode === 13) {
-                convertTemperature(kelvinSpan, fahrenheitSpan, inputCelsius);
-            }
-        });
-    }());
+    inputCelsius.addEventListener("keydown", function (e) {
+        if (e.keyCode === 13) {
+            convertTemperature(kelvinSpan, fahrenheitSpan, inputCelsius, warningP, notCorrectInputWarningClass);
+        }
+    });
+
+    inputCelsius.addEventListener("focus", function () {
+        removeWarningClass(warningP, inputCelsius, notCorrectInputWarningClass);
+    });
 });
 
