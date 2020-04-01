5c657cbaad24a6ec6d38a2c52532a0e312710e12
9045857 <9045857@mail.ru>
2020-04-01 17:27:52 +0700

    что-то подправил. Видимо сдела функция проверки на сохрание пустой задачи после редактирования.

--- L7T2Bootstrap/BootstrapTodoListJQueryScript.js
+++ L7T2Bootstrap/BootstrapTodoListJQueryScript.js
@@ -9,15 +9,15 @@
             "<div class='input-group mb-3'>" +
             "<div class='input-group-prepend'>" +
             "<div class='input-group-text'>" +
-            "<input type='checkbox' data-toggle='tooltip' title='Отметить выполнение задачи'>" +
+            "<input class='is-done-check' type='checkbox' data-toggle='tooltip' title='Отметить выполнение задачи'>" +
             "</div>" +
             "</div>" +
-            "<input type='text' class='form-control' disabled='disabled' aria-label='Text input with checkbox'>" +
+            "<input type='text' class='task-text form-control' disabled='disabled' aria-label='Text input with checkbox'>" +
             "<div class='input-group-append'>" +
-            "<button  type='button' class='btn btn-outline-secondary btn-light display-none' data-toggle='tooltip' title='Отменить редактирование'><i class='fas fa-remove-format'></i></button>" +
-            "<button  type='button' class='btn btn-outline-secondary btn-light display-none' data-toggle='tooltip' title='Сохранить редактирование'><i class='fas fa-save'></i></button>" +
-            "<button  type='button' class='btn btn-outline-secondary btn-light' data-toggle='tooltip'  title='Редактировать'><i class='far fa-edit'></i></button>" +
-            "<button  type='button' class='btn btn-outline-secondary btn-light' data-toggle='tooltip'  title='Удалить'><i class='fas fa-trash-alt'></i></button>" +
+            "<button type='button' class='not-save-btn btn btn-outline-secondary btn-light display-none' data-toggle='tooltip' title='Отменить редактирование'><i class='fas fa-remove-format'></i></button>" +
+            "<button type='button' class='save-btn btn btn-outline-secondary btn-light display-none' data-toggle='tooltip' title='Сохранить редактирование'><i class='fas fa-save'></i></button>" +
+            "<button type='button' class='correct-btn btn btn-outline-secondary btn-light' data-toggle='tooltip' title='Редактировать'><i class='far fa-edit'></i></button>" +
+            "<button type='button' class='delete-btn btn btn-outline-secondary btn-light' data-toggle='tooltip' title='Удалить'><i class='fas fa-trash-alt'></i></button>" +
             "</div>" +
             "</div>" +
             "</li>"
@@ -48,6 +48,11 @@
         liTextInput.prop("disabled", false);
     }
 
+    function isEmptyTask(task) {
+        var taskWithoutSpaces = task.replace(/\s+/g, "");
+        return taskWithoutSpaces === "";
+    }
+
     var tasksList = $("#tasks-list");
     var newTaskInput = $("#input-new-task");
     var addNewTaskButton = $("#input-task-button");
@@ -72,13 +77,12 @@
 
         var li = getNewTaskTr();
 
-        var inputs = li.find("input");
-        var liTextInput = inputs.eq(1);
+        var liTextInput = li.find(".task-text");
         liTextInput.val(newTaskText);
 
         tasksList.append(li);
 
-        var checkBox = inputs.eq(0);
+        var checkBox = li.find(".is-done-check");
         checkBox.click(function () {
             var lineThroughClass = "done";
             if (checkBox.prop("checked")) {
@@ -90,14 +94,20 @@
         checkBox.tooltip();
 
         var buttons = li.find("button");
-        var notSaveButton = buttons.eq(0);
-        var saveButton = buttons.eq(1);
-        var correctButton = buttons.eq(2);
-        var preDeleteButton = buttons.eq(3);
-
         buttons.tooltip();
 
+        var notSaveButton = li.find(".not-save-btn");
+        var saveButton = li.find(".save-btn");
+        var correctButton = li.find(".correct-btn");
+        var preDeleteButton = li.find(".delete-btn");
+
+        var inputTextBeforeCorrection;
+
         saveButton.click(function () {
+            if (isEmptyTask(liTextInput.val())) {
+                liTextInput.val(inputTextBeforeCorrection);
+            }
+
             closeSaveNotSaveButtons(saveButton,
                 notSaveButton,
                 correctButton,
@@ -105,8 +115,6 @@
                 displayNoneClass,
                 disabledClass);
         });
-
-        var inputTextBeforeCorrection;
 
         correctButton.click(function () {
             if (liTextInput.prop(disabledAttribute)) {
@@ -135,12 +143,11 @@
 
         preDeleteButton.click(function () {
             var deleteMenu = $("#deleteMenu");
-            deleteMenu.on("hidden.bs.modal",
-                function () {
-                    deleteMenu.modal("dispose");
-                });
+            deleteMenu.on("hidden.bs.modal", function () {
+                deleteMenu.modal("dispose");
+            });
 
-            var deleteButton = deleteMenu.find("button").eq(2);
+            var deleteButton = deleteMenu.find(".modal-footer .btn.btn-danger");
 
             deleteButton.click(function () {
                 li.remove();
