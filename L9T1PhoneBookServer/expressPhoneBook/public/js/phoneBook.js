dd327ba5a8fc94bbd5c24e61e40480ab00909cb5
9045857 <9045857@mail.ru>
2020-04-12 22:26:59 +0700

    форматирвовал книгу

---
+++ L9T1PhoneBookServer/expressPhoneBook/public/js/phoneBook.js
@@ -0,0 +1,43 @@
+﻿function PhoneBookService() {
+    this.getContacts = function(term) {
+        return $.get("/getContacts",
+            {
+                term:term
+            });
+    };
+
+    this.addContact = function (contact) {
+        //TODO
+    };
+
+    this.deleteContact = function (id) {
+        //TODO
+    };
+}
+
+new Vue({
+    el: "#app",
+    data: {
+        service: new PhoneBookService(),
+
+        contacts: [],
+        name: "",
+        phone: "",
+        term:""
+    },
+    methods: {
+        getContacts: function() {
+            this.service.getContacts(this.term);
+        },
+        deleteContact: function (c) {
+            //TODO
+        },
+        addContact: function() {
+            //TODO
+        },
+        search: function() {
+            this.getContacts();
+        }
+    }
+
+});
---
+++ L9T1PhoneBookServer/expressPhoneBook/routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+
+var contacts = [];
+//{id:1, name:"Ivan", phone: "876876" }
+var newId = 1;
+
+router.get('/getContacts', function (req, res) {
+    var term = (req.query.term || "").toUpperCase();
+
+    var filteredContacts = term.length === 0
+        ? contacts
+        : contacts.filter(function (c) {
+            return c.name.indexOf(term) >= 0
+                || c.phone.indexOf(term) >= 0;
+        });
+
+    res.send(filteredContacts);
+});
+
+router.post('/deleteContact', function(req, res) {
+    var id = req.body.id;
+
+    var contact = contacts.find(function (c) {
+        return c.id === id;
+    });
+
+    if (contact === undefined) {
+        res.send({
+            success: true,
+            message: "Contact with id=" + id + " not found."
+        });
+
+        return;
+    }
+
+    contacts = contacts.filter(function(c) {
+        return c.id !== id;
+    });
+
+    res.send({
+        success: true,
+        message:null
+    });
+});
+
+router.post("/addContact", function(req,res) {
+    var contact = req.body.request;
+
+    var hasContactWithPhone = contacts.some(function(c) {
+        return c.phone.toUpperCase() === contact.phone.toUpperCase;
+    });
+
+    if (hasContactWithPhone) {
+        res.send({
+            success: false,
+            message: "Contact with phone " + contact.phone+" already exists."
+        });
+
+        return;
+    }
+
+    contact.id = newId;
+    newId++;
+
+    contacts.push(contact);
+
+    res.send({
+        success: true,
+        message: null
+    });
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'PhoneBookServer' });
+});
+
+module.exports = router;
