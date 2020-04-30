62f7051991a4d1a8705c032612129f1ed6124223
9045857 <9045857@mail.ru>
2020-04-30 10:52:53 +0700

    готовый пример по vuetify

---
+++ L12T1Vuetify/vuetify/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <div>
+      Count: {{$store.state.count}}
+    </div>
+
+    <v-btn color="primary" @click="$store.commit('increment',2)">+2</v-btn>
+  </div>
+</template>
