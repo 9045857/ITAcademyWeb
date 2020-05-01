32231484d04a105935d584d6a52e9db7af5f5321
9045857 <9045857@mail.ru>
2020-05-01 11:53:53 +0700

    не видит компонент v-list

--- L12T1Vuetify/vuetify/src/App.vue
+++ L12T1Vuetify/vuetify/src/App.vue
@@ -1,74 +1,42 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+  <v-app class="app">
+    <v-container class="mt-2 mb-2 blue lighten-4" style="max-width: 800px;">
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-img alt="Vuetify Logo"
+               class="shrink mr-2"
+               contain
+               src="./assets/logo-vuetify.png"
+               transition="scale-transition"
+               width="40" />
+        <h1>
+          <span class="font-weight-thin mr-3">
+            L12T1
+          </span>
+          Задача "Vuetify"
+        </h1>
       </div>
+    </v-container>
+    <!--<v-spacer></v-spacer>-->
 
-      <v-spacer></v-spacer>
+    <TodoList style="background-color:blue;"></TodoList>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <v-tabs>
-        <v-tab>Item One</v-tab>
-        <v-tab>Item Two</v-tab>
-        <v-tab>Item Three</v-tab>
-      </v-tabs>
-
-      <div id="app">
-        <div id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about"> About</router-link>
-        </div>
-        <router-view />
-      </div>
-
-      <HelloWorld />
-    </v-content>
+    <v-footer absolute
+              class="font-weight-medium">
+      <v-col class="text-center"
+             cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+    TodoList
+  }
 }
 </script>
